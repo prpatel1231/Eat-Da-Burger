cd87262d5b5310f003efe937ca51d8045dfcbec2
prpatel <prpatel1231@gmail.com>
2020-03-17 10:57:00 -0400

    added schema and created models

---
+++ controllers/burger_controller.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req,res){
+    burger.all(function(data){
+        let hbsObject = {Burgers: data}
+        res.render("index", hbsObject)
+    })
+})
+
+router.post("/api/burgers", function(req,res){
+    burger.create(["burger_name"],[req.body.burgerName],
+        function(result){
+            res.json({id: result.insertId})
+    })
+})
+
+router.put("/api/burgers/:id", function(req, res){
+    let condition = "id = " + req.params.id
+    burger.update({devoured: req.body.devouredVal},condition,
+        function(result){
+            if(result.changedRows == 0) return res.status(404).end()
+            return res.status(200).end()
+        })
+})
+
+router.delete("/api/burgers/:id", function(req,res){
+    let condition = "id = " + req.params.id
+    burger.delete(condition, 
+        function(result){
+            if(result.affectedRows == 0) return res.status(404).end()
+            return res.status(200).end()
+    })
+})
+
+module.exports = router;
---
+++ public/assets/js/burger.js
@@ -0,0 +1,72 @@
+$(function() {
+    function checkEmpty(){
+        let liToEat = $(".liToEat");
+        let liAte = $(".liAte");
+
+        if(liToEat.length) {$(".left-div ul h3").text("Ready to Eat..")}
+        else {$(".left-div ul h3").text("Add some burgers to Enjoy...")}
+        
+        if(liAte.length) {$(".right-div ul h3").text("Woww..You are doing great!")}
+        else {$(".right-div ul h3").text("Hungry!!!")}
+    }
+
+    $(".create-form").on("submit", function(event){
+        event.preventDefault()
+        
+        if($("#burgName").val()){
+            let re = /(\b[a-z](?!\s))/g;
+            let burgName = $("#burgName").val().trim()
+            burgName = burgName.replace(re, (x) => {return x.toUpperCase()})
+            
+            let newBurger = {
+                burgerName: burgName
+            }
+            
+            $.ajax("api/burgers", {
+            type: "POST",
+            data: newBurger
+            }).then(
+                function(){
+                    console.log("New burger added")
+                    location.reload()
+                }
+            )
+        }
+    })
+
+    $(".eatNoeat").on("click", function(event){
+        let id = $(this).data("id")
+        let devoured = !($(this).data("devour"))
+
+        let newDevoured = {
+            devouredVal: devoured
+        }
+        
+        $.ajax("api/burgers/" + id,{
+            type: "PUT",
+            data: newDevoured
+        }).then(
+            function(){
+                console.log("Burger devoured to", devoured)
+                location.reload()
+            }
+        )
+    })
+
+    $(".delete").on("click", function(event){
+        let id = $(this).data("id")
+        let reply = confirm("Are you sure about removing " + $(this).data("name") + " from the list and Database?");
+        
+        if(reply){
+            $.ajax("api/burgers/" + id,{
+                type: "DELETE",
+            }).then(
+                function(){
+                    console.log("deleted the burger ", id)
+                    location.reload()
+                }
+            )
+        }
+    })
+    checkEmpty()
+})
